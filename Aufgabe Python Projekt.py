1f34c8a17c80a8bda1f2f1cd00ef8c7821dd0c70
DomeDevOps <ads4me@ik.me>
2025-04-09 19:37:46 +0200

    Test

--- Aufgabe Python Projekt.py
+++ Aufgabe Python Projekt.py
@@ -20,13 +20,13 @@ elif auswahl != "0":
 
 
 
-Solange die Zahl nicht 0 ist, machen wir weiter
-while zahl != 0:
-    # Wir rechnen die Zahl zur Summe dazu
-    summe = summe + a_zahl
-
-    # Wir fragen nach der nächsten Zahl
-    zahl = int(input("Bitte gib eine weitere Zahl ein (0 zum Beenden): "))
+# Solange die Zahl nicht 0 ist, machen wir weiter
+# while zahl != 0:
+#     # Wir rechnen die Zahl zur Summe dazu
+#     summe = summe + zahl
+#
+#     # Wir fragen nach der nächsten Zahl
+#     zahl = int(input("Bitte gib eine weitere Zahl ein (0 zum Beenden): "))
 
 # Wenn der Benutzer 0 eingibt, sind wir fertig
 # Jetzt zeigen wir das Ergebnis
